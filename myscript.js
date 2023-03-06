c370fed1c801516ea5c02f5aa593e36b58c6fe4e
lurilou <126409792+lurilou@users.noreply.github.com>
2023-03-06 23:20:31 +0100

    Add scorekeeping and game(), fixed problems
    Function for looping the game five times added. Then realised that the 'random' number returned is always the same. Quick google search revealed, that the function was nested too dep so fixed that. Also realised I had forgotten to add conditions for the player choosing scissors, so added those. Finally, score adjustment was added and the statements adjusted.

--- myscript.js
+++ myscript.js
@@ -1,10 +1,28 @@
 //define variables for playerSelection, computerSelection, playerScore, computerScore
-let computerSelection = getComputerChoice();
-let playerSelection;
 
+let playerScore = 0;
+let computerScore = 0;
 
 //loop to play game five times
-
+function game() {
+    for (i=0; i<5; i++) {
+        //get player guess
+    let input = prompt('Rock, paper, Scissors?');
+    let playerSelection = input.toLowerCase();
+    let computerSelection=getComputerChoice();
+    console.log('You chose ' + playerSelection + ' and the computer chose ' + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore === computerScore) {
+       console.log(`It's a tie! Final scores: ${playerScore} (player) to ${computerScore} (computer).`) 
+    }
+    else if (playerScore < computerScore) {
+        console.log(`You lost, big time! Final scores: ${playerScore} (player) to ${computerScore} (computer).`)
+    }
+    else {
+        console.log(`You're a winner, baby! Final scores: ${playerScore} (player) to ${computerScore} (computer).`)
+    }
+}
 //get computer guess
 function getComputerChoice () {
     let random = Math.floor(Math.random()*3);
@@ -18,28 +36,38 @@ function getComputerChoice () {
 
 
 function playRound (playerSelection, computerSelection){
-//get player guess
-    let input = prompt('Rock, paper, Scissors?');
-    playerSelection = input.toLowerCase();
-    getComputerChoice();
 // compare the guesses and determine winner
     if (playerSelection === computerSelection) {
-        console.log('It\'s a tie')
+        return `It's a tie. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
     }
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
-            return 'You lose! Paper covers rock.';
+            computerScore++;
+            return `You lose! Paper covers rock. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
         }
         else {
-            return 'You win! Rock crushes scissors.';
+            playerScore++;
+            return `You win! Rock crushes scissors. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            return 'You win! Paper covers rock.';
+            playerScore++;
+            return `You win! Paper covers rock. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
         }
         else {
-            return 'You lose! Scissors cut paper.';
+            computerScore++;
+            return `You lose! Scissors cut paper. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
+        }
+    }
+    else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            computerScore++;
+            return `You lose! Rock crushes scissors. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
+        }
+        else {
+            playerScore++;
+            return `You win! Scissors cut paper. Current score : ${playerScore} (player) to ${computerScore} (computer).`;
         }
     }
     else {
@@ -47,7 +75,6 @@ function playRound (playerSelection, computerSelection){
     }
 }
 
-
-console.log(playRound(playerSelection, computerSelection));
+game()
 
 //update scores
