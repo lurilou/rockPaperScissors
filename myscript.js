a080bfaea7b4531d6b58a2e744e00efa81eaf4dd
lurilou <126409792+lurilou@users.noreply.github.com>
2023-03-06 22:19:43 +0100

    Add function for on round Also changed functions to return value

--- myscript.js
+++ myscript.js
@@ -1,19 +1,53 @@
-//define variables for playerGuess, computerGuess, playerScore, computerScore
-let computerGuess = 0;
-let playerGuess = 0;
+//define variables for playerSelection, computerSelection, playerScore, computerScore
+let computerSelection = getComputerChoice();
+let playerSelection;
+
+
 //loop to play game five times
+
 //get computer guess
 function getComputerChoice () {
     let random = Math.floor(Math.random()*3);
         if (random === 0) {
-            computerGuess = 'Rock';}
+            return 'rock';}
         else if (random === 1) {
-            computerGuess = 'Paper';}
+            return 'paper';}
         else {
-            computerGuess = 'Scissors';}
+            return 'scissors';}
 }
 
-getComputerChoice();
+
+function playRound (playerSelection, computerSelection){
 //get player guess
+    let input = prompt('Rock, paper, Scissors?');
+    playerSelection = input.toLowerCase();
+    getComputerChoice();
 // compare the guesses and determine winner
+    if (playerSelection === computerSelection) {
+        console.log('It\'s a tie')
+    }
+    else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You lose! Paper covers rock.';
+        }
+        else {
+            return 'You win! Rock crushes scissors.';
+        }
+    }
+    else if (playerSelection === 'paper') {
+        if (computerSelection === 'rock') {
+            return 'You win! Paper covers rock.';
+        }
+        else {
+            return 'You lose! Scissors cut paper.';
+        }
+    }
+    else {
+        return 'Invalid input!';
+    }
+}
+
+
+console.log(playRound(playerSelection, computerSelection));
+
 //update scores
